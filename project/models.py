4a2e3c7e704c87c75dbf45a5d64eddb3b2a1ceab
brunoqs <brun_o13@hotmail.com>
2019-06-28 17:37:21 -0300

    datefield add

--- project/models.py
+++ project/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField("Nome", max_length=50)
     abstract = models.TextField("Resumo")
-    year_beg = models.IntegerField()
-    year_end = models.IntegerField()
+    year_beg = models.DateField()
+    year_end = models.DateField()
     paper = models.FileField(upload_to='papers/')
 
     def __str__(self):
